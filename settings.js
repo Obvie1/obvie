8a6a1d3cad7202aaaee4e02d370c08353cf1e0a2
Obvie1 <ngwaneobvious@gmail.com>
2025-08-17 07:39:00 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'bot‎',
   botName: "obvie Bot",
   botOwner: 'obvious Ngwane ', // Your name
-  ownerNumber: '2630780568114', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '263780568114', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
